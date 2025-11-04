55a7c037a1029b9facfa7d6584f059eb899c557a
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-11-04 08:33:26 -0500

    Initial Commit

---
+++ MediaPlayerApp/MediaPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerAppApp.swift
+//  MediaPlayerApp
+//
+//  Created by Fatom on 2025-11-04.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaPlayerAppTests/MediaPlayerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerAppTests.swift
+//  MediaPlayerAppTests
+//
+//  Created by Fatom on 2025-11-04.
+//
+
+import Testing
+@testable import MediaPlayerApp
+
+struct MediaPlayerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
